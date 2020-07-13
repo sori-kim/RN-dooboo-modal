6b31b44cb79c09d34a9e71e953aa3f64a117d2ae
rimuuu <soryonew@gmail.com>
2020-07-13 20:20:37 +0900

    [Add] Created README.md - gives basic intruduction to Modal component.

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -4,65 +4,88 @@ import { Text, TouchableOpacity, TextStyle, ViewStyle } from 'react-native';
 import Modal from './Modal';
 
 interface Props {
-    style? : ViewStyle | object | undefined;
-    textStyle? : TextStyle | object | undefined;
+  style?: ViewStyle | undefined;
+  textStyle?: TextStyle | undefined;
 }
 
 function ModalPage(props: Props) {
-    const {
-        style,
-        textStyle
-    } = props
-    
-    const [modalVisible, setModalVisible] = useState<boolean>(false)
-    const [isAnimated, setIsAnimated] = useState<boolean>(false)
-    const [value, setValue] = useState("Choose!");
+  const { style, textStyle } = props;
 
-    const openModal = () => {
-      setModalVisible(true)
-    }
-    const closeModal = () => {
-      setModalVisible(false)
-    }
+  const [modalVisible, setModalVisible] = useState<boolean>(false);
+  const [isAnimated, setIsAnimated] = useState<boolean>(false);
+  const [value, setValue] = useState('DoobooLab');
 
-    const data = [
-      { section: true, label: 'Fruits' },
-      { label: 'Red Apples' },
-      { label: 'Cherries' },
-      { label: 'Cranberries', accessibilityLabel: 'Tap here for cranberries' },
-      { label: 'Vegetable', customKey: 'Not a fruit' }
+  const openModal = (): void => {
+    setModalVisible(true);
+  };
+  const closeModal = (): void => {
+    setModalVisible(false);
+  };
+
+  const data = [
+    { section: true, label: 'DoobooLab' },
+    { label: 'Hyo' },
+    { label: 'Daniel' },
+    { label: 'Sarah' },
+    { label: 'Clark' },
+    { label: 'Dean' },
+    { label: 'Rosie' },
+    { label: 'Song' },
   ];
 
-    return (
-      <>
-        <TouchableOpacity 
-             style={[style,{width: 200, height: 80, backgroundColor: '#088EDF', borderRadius: 5, justifyContent: 'center', alignItems: 'center', zIndex: -9 }]}
-             onPress={() => { setIsAnimated(true); openModal();  } }>
-            <Text style={[ textStyle, {color: '#ffff', fontWeight: '600', fontSize: '17px'}]}>Open Modal</Text>
-        </TouchableOpacity>
-        {
-          modalVisible &&
-           <Modal
-            visible={modalVisible}
-            closable={true}
-            backDropClosable={true}
-            onClose={closeModal}
-            // style={{backgroundColor: '#ffff'}}
-            isAnimated={isAnimated} 
-            animationType={"default"}
-            animationSpeed={700}
-            data={data}
-            initialValue={value}
-            >
-             {/* <Text 
-                style={[ textStyle, {color: "#088EDF", fontWeight: '600', textAlign: 'center'}]}>
-                Hello, I am Modal 🤘🏻
-             </Text>   */}
-          </Modal>
-        }
-      </>
-    )
-  }
+  return (
+    <>
+      <TouchableOpacity
+        style={[
+          {
+            width: 250,
+            height: 70,
+            backgroundColor: '#088EDF',
+            borderRadius: 5,
+            justifyContent: 'center',
+            alignItems: 'center',
+            zIndex: -9,
+          },
+        ]}
+        onPress={() => {
+          setIsAnimated(true);
+          openModal();
+        }}>
+        <Text
+          style={[
+            textStyle,
+            { color: '#ffff', fontWeight: '600', fontSize: '17px' },
+          ]}>
+          {value}
+        </Text>
+      </TouchableOpacity>
+      {modalVisible && (
+        <Modal
+          visible={modalVisible}
+          closable={true}
+          backDropClosable={true}
+          onClose={closeModal}
+          style={style}
+          isAnimated={isAnimated}
+          animationType={'default'}
+          animationSpeed={700}
+          mode={'default'}
+          data={data}
+          initialValue={value}
+          value={value}
+          setValue={setValue}>
+          <Text
+            style={[
+              textStyle,
+              { color: '#088EDF', fontWeight: '600', textAlign: 'center' },
+            ]}>
+            Hello, I am Modal 🤘🏻
+          </Text>
+        </Modal>
+      )}
+    </>
+  );
+}
 
   export default ModalPage;
 
--- src/components/NewModalPicker/ModalPicker.tsx
+++ src/components/NewModalPicker/ModalPicker.tsx
@@ -2,21 +2,25 @@ import React, { useState } from 'react';
 import { View, TextInput, Text, Picker} from 'react-native';
 import styled from 'styled-components/native';
 
-
-
-
 function ModalPicker (props){
   const [options, setOptions] = useState(props.data);   
-  const [value, setValue] = useState('Select Your Choice!')
+
+  const {
+    value,
+    setValue
+  } = props
+
+  console.log("modalPicker value>>", value)
 
 
 
     return(
-      <Container>
+      <>
       <Picker
        selectedValue={value}
-       onValueChange={(itemVale, itemIndex)=>{
-         setValue(itemVale)
+       onValueChange={(itemValue, itemIndex)=>{
+         setValue(itemValue)
+         alert(`Selected!`)
        }}
       >
         {
@@ -25,18 +29,9 @@ function ModalPicker (props){
         })
        }
       </Picker>
-      <Selected>
-        <Text style={{fontSize: 20, fontWeight: '400' }} >{value}🧡</Text>
-      </Selected>
-     </Container> 
+     </> 
     )
 }
 
 export default ModalPicker;
 
-const Container = styled.View``;
-
-const Selected = styled.View`
- justify-content: center;
- align-items: center;
-`;
